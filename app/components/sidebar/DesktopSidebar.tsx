1e91c9dc2484ba0e0bd16abd364362cb3c15ed85
Beqael <beqaelashvili3@gmail.com>
2025-01-28 10:55:22 +0400

    add_getCurrentUsers

--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -1,12 +1,20 @@
 "use client";
 
-import useRoutes from "@/app/hooks/useRoutes";
 import { useState } from "react";
+import useRoutes from "@/app/hooks/useRoutes";
 import DesktopItem from "./DesktopItem";
 
-const DesktopSidebar = () => {
+import { User } from "@prisma/client";
+
+interface DesktopSidebarProps {
+  currentUser: User;
+}
+
+const DesktopSidebar: React.FC<DesktopSidebarProps> = ({ currentUser }) => {
   const routes = useRoutes();
   const [isOpen, setISOpen] = useState<boolean>(false);
+
+  console.log({ currentUser });
   return (
     <div className="hidden lg:flex lg:inset-y-0 lg:left-0 lg:z-40 lg:w-20 lg:px-6 lg:overflow-y-auto lg:bg-white lg:border-r-[1px] lg:pb-4 lg:flex-col justify-between">
       <nav className="mt-4 flex flex-col justify-between">
--- app/components/sidebar/MobileFooter.tsx
+++ app/components/sidebar/MobileFooter.tsx
@@ -1,7 +1,25 @@
 "use client";
 
+import useRoutes from "@/app/hooks/useRoutes";
+import useConservation from "@/app/hooks/useConversation";
+import MobileItem from "./MobileItem";
+
 const MobileFooter = () => {
-  return <div>this is mobile footer</div>;
+  const routes = useRoutes();
+  const { isOpen } = useConservation();
+  return (
+    <div className="fixed justify-center w-full bottom-0 z-40 flex items-center bg-white border-t-[1px] lg:hidden">
+      {routes.map((route) => (
+        <MobileItem
+          key={route.href}
+          href={route.href}
+          active={route.active}
+          icon={route.icon}
+          onClick={route.onClick ? route.onClick : () => {}}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default MobileFooter;
