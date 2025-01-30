fce7b460343bc2da73c8883573dc1a072c9c2f80
Beqael <beqaelashvili3@gmail.com>
2025-01-30 13:36:44 +0400

    add-user_uploud_images,send-messages_seenByuser

--- app/components/sidebar/MobileFooter.tsx
+++ app/components/sidebar/MobileFooter.tsx
@@ -7,6 +7,10 @@ import MobileItem from "./MobileItem";
 const MobileFooter = () => {
   const routes = useRoutes();
   const { isOpen } = useConversation();
+
+  if (isOpen) {
+    return null;
+  }
   return (
     <div className="fixed justify-center w-full bottom-0 z-40 flex items-center bg-white border-t-[1px] lg:hidden">
       {routes.map((route) => (
